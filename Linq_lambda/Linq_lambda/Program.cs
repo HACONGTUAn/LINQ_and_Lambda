3e596f5653eedf4b29b5ffc030b90b60d3648831
HACONGTUAn <drt5tuan@gmail.com>
2024-04-22 11:00:46 +0700

    thuc hanh Lambda

---
+++ Linq_lambda/Linq_lambda/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Linq_lambda
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
